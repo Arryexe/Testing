a927175981e016528b5e1175c08c77d801cb9432
Arryexe <arianur098@gmail.com>
2019-03-20 11:51:45 +0800

    Designing Tasks Index with Bootstrap

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,23 +1,35 @@
-<html>
-	<head>
-		<title>Tasks Management</title>
-	</head>
+@extends('layouts.app')
 
-	<body>
-		<form action="{{ url('tasks') }}" method="post">
-			{{ csrf_field() }}
-			<input type="text" name="name">
-			<textarea name="description"></textarea>
-			<input type="submit" value="Create Task">
-		</form>
-
-		<h1>Tasks Management</h1>
-		<ul>
-			@foreach ($tasks as $task)
-				Tugas Pertama : {{ $task->name }} <br>
-				-{{ $task->description }}
-				<br>
-			@endforeach
-		</ul>
-	</body>
-</html>
+@section('content')
+	<h1 class="page-header text-center">Tasks Management</h1>
+	<div class="py-4">
+		<div class="row">
+		    <div class="col-md-4 col-md-offset-2">
+		        <h2>Tasks</h2>
+		        <ul class="list-group">
+		            @foreach ($tasks as $task)
+		                <li class="list-group-item">
+		                    {{ $task->name }} <br>
+		                    {{ $task->description }}
+		                </li>
+		            @endforeach
+		        </ul>
+		    </div>
+		    <div class="col-md-4">
+		        <h2>New Task</h2>
+		        <form action="{{ url('tasks') }}" method="post">
+		            {{ csrf_field() }}
+		            <div class="form-group">
+		                <label for="name" class="control-label">Name</label>
+		                <input id="name" name="name" class="form-control" type="text">
+		            </div>
+		            <div class="form-group">
+		                <label for="description" class="control-label">Description</label>
+		                <textarea id="description" name="description" class="form-control"></textarea>
+		            </div>
+		            <input type="submit" value="Create Task" class="btn btn-primary">
+		        </form>
+		    </div>
+		</div>
+	</div>
+@endsection
